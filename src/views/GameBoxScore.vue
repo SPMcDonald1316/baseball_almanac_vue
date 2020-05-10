83793372931a34cef4bab179e4a0a01cdc944ff8
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-10 12:33:28 -0500

    start displaying game play by play data

--- src/views/GameBoxScore.vue
+++ src/views/GameBoxScore.vue
@@ -2,7 +2,30 @@
   <div class="franchises-show">
     <h1>{{ message }}</h1>
     <div v-bind:key="event.id" v-for="event in game">
-      <p>{{event}}</p>
+      <table>
+        <thead>
+          <th>Inning</th>
+          <th>Team Batting</th>
+          <th>Batter</th>
+          <th>Pitcher</th>
+          <th>Outs</th>
+          <th>Balls</th>
+          <th>Strikes</th>
+          <th>Event</th>
+        </thead>
+        <tbody>
+          <tr v-bind:key="event.id" v-for="event in game">
+            <td>{{ event.inning }}</td>
+            <td>{{ event.batting }}</td>
+            <td>{{ event.batter }}</td>
+            <td>{{ event.pitcher }}</td>
+            <td>{{ event.outs }}</td>
+            <td>{{ event.balls }}</td>
+            <td>{{ event.strikes }}</td>
+            <td>{{ event.event }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
