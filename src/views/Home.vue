07e903ac7d003ff30fbb7548d586bc99c4626078
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-11 10:52:56 -0500

    add home page functionality for searching teams and start functionality for players

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div>
+      <h3>Teams</h3>
+      <p>Search Teams: <input type="text" v-model="franchise" list="franchises"></p>
+      <datalist id="franchises">
+        <option v-bind:key="franchise.id" v-for="franchise in franchises" v-bind:value="franchise.franch_id">{{franchise.franch_name}}</option>
+      </datalist>
+      <router-link :to="{ name: 'franchises-show', params: { franch_id: franchise } }" tag="button">Search Teams</router-link>
+      <router-link :to="{ name: 'franchises-index'}" tag="button">See All Teams</router-link>
+    </div>
+    <div>
+      <h3>Players</h3>
+      <p>Search Teams: <input type="text" v-model="player" list="players"></p>
+      <datalist id="players">
+        <option v-bind:key="player.id" v-for="player in players" v-bind:value="player.id">{{ player.name }}</option>
+      </datalist>
+      <router-link :to="{ name: 'players-show', params: { id: player } }" tag="button">Search Player</router-link>
+      <!-- <router-link :to="{ name: 'franchises-index'}" tag="button">See All Teams</router-link> -->
+    </div>
+    <div>
+      <h3>Games</h3>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Baseball Almanac",
+      franchises: {},
+      franchise: "",
+      lastNameStartsWith: {},
+      player: ""
+    };
+  },
+  created: function() {
+    axios.get('/api/franchises').then(response => {
+      this.franchises = response.data;
+      axios.get('api/players').then(response => {
+        this.players = response.data;
+        console.log(this.players);
+      });
+    });
+  },
+  methods: {}
+};
 </script>
