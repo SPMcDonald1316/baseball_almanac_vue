214c55ab86dcc964136f08ef86d552108c991821
Sean McDonald <spmcdonald1316@gmail.com>
2020-05-14 15:03:22 -0500

    start game event page

--- src/views/GameBoxScore.vue
+++ src/views/GameBoxScore.vue
@@ -4,7 +4,7 @@
       <!-- Inner -->
       <div class="inner">
         <header>
-          <h1><a href="index.html" id="logo">Helios</a></h1>
+          <h1>Baseball Almanac</h1>
         </header>
       </div>
       <!-- Nav -->
@@ -99,113 +99,41 @@
           <div class="col-8 col-12-mobile imp-mobile" id="content">
             <article id="main">
               <header>
-                <h2><a href="#">Left Sidebar</a></h2>
-                <p>
-                  Morbi convallis lectus malesuada sed fermentum dolore amet
-                </p>
+                <!-- would like to pass this info down from franchisestats page -->
+                <h2>Game</h2>
+                <p>Date</p>
               </header>
-              <a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
-              <p>
-                Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa
-                posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus
-                lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum
-                consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue
-                interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia
-                natoque aenean scelerisque vel lacinia mollis quam sodales congue.
-              </p>
               <section>
-                <header>
-                  <h3>Ultrices tempor sagittis nisl</h3>
-                </header>
-                <p>
-                  Nascetur volutpat nibh ullamcorper vivamus at purus. Cursus ultrices porttitor sollicitudin imperdiet
-                  at pretium tellus in euismod a integer sodales neque. Nibh quis dui quis mattis eget imperdiet venenatis
-                  feugiat. Neque primis ligula cum erat aenean tristique luctus risus ipsum praesent iaculis. Fermentum elit
-                  fringilla consequat dis arcu. Pellentesque mus tempor vitae pretium sodales porttitor lacus. Phasellus
-                  egestas odio nisl duis sociis purus faucibus morbi. Eget massa mus etiam sociis pharetra magna.
-                </p>
-                <p>
-                  Eleifend auctor turpis magnis sed porta nisl pretium. Aenean suspendisse nulla eget sed etiam parturient
-                  orci cursus nibh. Quisque eu nec neque felis laoreet diam morbi egestas. Dignissim cras rutrum consectetur
-                  ut penatibus fermentum nibh erat malesuada varius.
-                </p>
-              </section>
-              <section>
-                <header>
-                  <h3>Augue euismod feugiat tempus</h3>
-                </header>
-                <p>
-                  Pretium tellus in euismod a integer sodales neque. Nibh quis dui quis mattis eget imperdiet venenatis
-                  feugiat. Neque primis ligula cum erat aenean tristique luctus risus ipsum praesent iaculis. Fermentum elit
-                  ut nunc urna volutpat donec cubilia commodo risus morbi. Lobortis vestibulum velit malesuada ante
-                  egestas odio nisl duis sociis purus faucibus morbi. Eget massa mus etiam sociis pharetra magna.
-                </p>
+                <table class="default">
+                  <thead>
+                    <th>Inning</th>
+                    <th>Team Batting</th>
+                    <th>Batter</th>
+                    <th>Pitcher</th>
+                    <th>Outs</th>
+                    <th>Balls</th>
+                    <th>Strikes</th>
+                    <th>Event</th>
+                  </thead>
+                  <tbody>
+                    <tr v-bind:key="event.id" v-for="event in game">
+                      <td>{{ event.inning }}</td>
+                      <td>{{ event.batting }}</td>
+                      <td>{{ event.batter }}</td>
+                      <td>{{ event.pitcher }}</td>
+                      <td>{{ event.outs }}</td>
+                      <td>{{ event.balls }}</td>
+                      <td>{{ event.strikes }}</td>
+                      <td>{{ event.event }}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </section>
             </article>
           </div>
         </div>
-        <hr />
-        <div class="row">
-          <article class="col-4 col-12-mobile special">
-            <a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
-            <header>
-              <h3><a href="#">Gravida aliquam penatibus</a></h3>
-            </header>
-            <p>
-              Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-              porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-            </p>
-          </article>
-          <article class="col-4 col-12-mobile special">
-            <a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
-            <header>
-              <h3><a href="#">Sed quis rhoncus placerat</a></h3>
-            </header>
-            <p>
-              Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-              porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-            </p>
-          </article>
-          <article class="col-4 col-12-mobile special">
-            <a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
-            <header>
-              <h3><a href="#">Magna laoreet et aliquam</a></h3>
-            </header>
-            <p>
-              Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
-              porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
-            </p>
-          </article>
-        </div>
       </div>
 
-    </div>
-    <h1>{{ message }}</h1>
-    <div>
-      <table>
-        <thead>
-          <th>Inning</th>
-          <th>Team Batting</th>
-          <th>Batter</th>
-          <th>Pitcher</th>
-          <th>Outs</th>
-          <th>Balls</th>
-          <th>Strikes</th>
-          <th>Event</th>
-        </thead>
-        <tbody>
-          <tr v-bind:key="event.id" v-for="event in game">
-            <td>{{ event.inning }}</td>
-            <td>{{ event.batting }}</td>
-            <td>{{ event.batter }}</td>
-            <td>{{ event.pitcher }}</td>
-            <td>{{ event.outs }}</td>
-            <td>{{ event.balls }}</td>
-            <td>{{ event.strikes }}</td>
-            <td>{{ event.event }}</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   </div>
 </template>
